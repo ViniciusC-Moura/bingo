bde65fbcc5b6231927cd6085965c380c00c3af4f
THIAG0ST0S0 <t.thiago@escolar.ifrn.edu.br>
2025-01-28 21:07:24 -0300

    Criação da função imprimir cartela

--- main.py
+++ main.py
@@ -12,4 +12,13 @@ def gerar_cartela(modo):
                  random.sample(range(11, 21), 3),
                  random.sample(range(21, 31), 3),
                  random.sample(range(31, 41), 3)]]
-    
+
+def imprimir_cartela(cartela, numeros_sorteados, modo):
+    if modo == 'rápido':
+        qtd = 10
+    else:
+        qtd = 18
+    print("-" * qtd)
+    for linha in zip(*cartela):
+        print(" | ".join(f"({num:02})" if num in numeros_sorteados else f" {num:02} " for num in sum(linha, [])))
+    print("-" * qtd)
